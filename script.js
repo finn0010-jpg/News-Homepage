c0a278ae475903f0a54a08035fc51616089cf8b6
Caleb Stanfield <finn0010@gmail.comm>
2026-03-05 17:00:02 -0600

    Added menu functionality for mobile

---
+++ script.js
@@ -0,0 +1,11 @@
+const openMenu = document.getElementById('open-menu');
+const closeMenu = document.getElementById('close-menu');
+const menu = document.getElementById('nav');
+
+openMenu.addEventListener('click', function() {
+    menu.classList.toggle('hidden');
+});
+
+closeMenu.addEventListener('click', function() {
+    menu.classList.toggle('hidden');
+});
